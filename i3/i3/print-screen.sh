1be121c249cd68edc54160a16395dee901064ae3
Nils Vreman <vrn1lud@bosch.com>
2025-02-18 14:24:26 +0100

    fix(print): Fixed printscreen

--- i3/i3/print-screen.sh
+++ i3/i3/print-screen.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-tmpfile=$(mktemp)
+# Setup
 format='.png'
-tmpscreenshot="${tmpfile}${format}"
-targetdir=${1:-"$HOME/Pictures/Screenshots"}
 datetime=$(date +"%Y-%m-%d_%H-%M-%S")
+
+tmpfile=$(mktemp)
+tmpscreenshot="${tmpfile}${format}"
+
+targetdir=${1:-"$HOME/Pictures/Screenshots"}
 targetfile="$targetdir/screenshot-$datetime$format"
 
+# Extract focused window id from i3's tree
+focused_window=$(i3-msg -t get_tree | jq '.. | select(.focused? == true).window' | head -n 1)
+
+#
 mv "$tmpfile" "$tmpscreenshot"
-import -window root "$tmpscreenshot"
+import -window "$focused_window" "$tmpscreenshot"
 cp "$tmpscreenshot" "$targetfile"
 rm "$tmpscreenshot"
-xclip -i <"$targetfile"
+notify-send -t 5000 "Screenshot saved to $targetfile"
+xclip -selection clipboard -target image/png -i "$targetfile"
