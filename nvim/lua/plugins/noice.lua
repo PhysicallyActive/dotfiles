e7887891e5f14096a9a2c0a66976709ba0c432c9
Nils Vreman <vrn1lud@bosch.com>
2025-08-27 16:33:58 +0200

    feat(nvim): borders around popups, completions, etc.

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,13 +1,32 @@
--- Used to ignore "No information available" messages from noice when multiple LSPs are attached to same buffer
+-- For views, notifications, popups, etc.
 return {
   "folke/noice.nvim",
-  opts = function(_, opts)
-    table.insert(opts.routes, {
-      filter = {
-        event = "notify",
-        find = "No information available",
+  opts = {
+    presets = {
+      lsp_doc_border = true,
+    },
+    routes = {
+      {
+        filter = { -- Used to ignore "No information available" messages on multiple LSPs attached to same buffer
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
       },
-      opts = { skip = true },
-    })
-  end,
+    },
+    views = {
+      hover = {
+        border = { style = "rounded" },
+        win_options = {
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+        },
+      },
+      signature = {
+        border = { style = "rounded" },
+        win_options = {
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+        },
+      },
+    },
+  },
 }
