566b0d95df95ababfed7373f0771cbc682722e82
Nils Vreman <vrn1lud@bosch.com>
2025-12-19 14:51:51 +0100

    fix(kitty): the kitty-scrollback commands messed with normal keybindings so moved them to autocmd

--- nvim/lua/plugins/kitty.lua
+++ nvim/lua/plugins/kitty.lua
@@ -10,29 +10,6 @@ return {
   keys = {
     { "<C-CR>", false }, -- Disable execute in all modes
     { "<S-CR>", false }, -- Disable paste to terminal in all modes
-    {
-      "<leader><CR>",
-      "<Plug>(KsbExecuteVisualCmd)",
-      mode = "v",
-      desc = "Execute the contents of visual selection in Kitty",
-    },
-    {
-      "<leader><CR>",
-      "<Plug>(KsbExecuteCmd)",
-      mode = { "n", "i" },
-      desc = "Execute the contents of the paste window in Kitty",
-    },
-    {
-      "<leader>p",
-      "<Plug>(KsbPasteVisualCmd)",
-      mode = "v",
-      desc = "Paste the contents of visual selection to Kitty without executing",
-    },
-    {
-      "<leader>p",
-      "<Plug>(KsbPasteCmd)",
-      mode = { "n", "i" },
-      desc = "Paste the contents of the paste window to Kitty without executing",
-    },
+    -- keybindings are added as autocmds in nvim/lua/config/autocmds.lua
   },
 }
